e8502d3f8fafaeecdfee3a42e9fe3d01ca83642a
Piotr Kopycki <pkopycki@onet.pl>
2022-02-17 13:25:18 +0100

    Init commit

---
+++ src/test/java/com/example/simplestoragesystem/SimpleStorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplestoragesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStorageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
